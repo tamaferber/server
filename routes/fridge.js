9a71dae7237c8d6a41436ae794e49b21fbd2ea18
Tama Ferber <tamaferber@gmail.com>
2025-07-09 16:57:40 +0300

    insert fridge items to database

---
+++ routes/fridge.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const router = express.Router();
+const FridgeItem = require('../models/fridgeItem');
+
+router.post('/', async (req, res) => {
+  try {
+    const { items } = req.body;
+
+    if (!items || !Array.isArray(items)) {
+      return res.status(400).json({ message: 'Invalid items format' });
+    }
+
+    // המרה של expirationDate ממחרוזת ל- Date
+    const parsedItems = items.map(item => ({
+      ...item,
+      expirationDate: new Date(item.expirationDate)
+    }));
+
+    //testing delete
+    console.log("Parsed items to insert:", parsedItems);
+    await FridgeItem.insertMany(parsedItems);
+
+    res.status(201).json({ message: 'Items saved to fridge' });
+  } catch (err) {
+    console.error('Error saving items:', err);
+    res.status(500).json({ message: 'Server error saving items' });
+  }
+});
+
+
+module.exports = router;
