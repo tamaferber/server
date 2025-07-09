712f5cac39c19d5e03b7851bd9ce092844479cac
Tama Ferber <tamaferber@gmail.com>
2025-07-09 19:12:36 +0300

    fix some wrong line

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const fridgeRoute = require('./routes/fridge');
 require('./db');  //connection to Database
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(bodyParser.json());
