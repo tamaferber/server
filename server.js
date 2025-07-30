aa2435e7e15b4ef98d786efe760a547a62bb28b3
Tama Ferber <tamaferber@gmail.com>
2025-07-30 15:06:04 +0300

    debuaging

--- server.js
+++ server.js
@@ -1,18 +1,18 @@
-// const express = require('express');
-// const cors = require('cors');
-// const bodyParser = require('body-parser');
-// const fridgeRoute = require('./routes/fridge'); 
-// const usersRouter = require('./routes/users'); 
-// const adminRouter = require('./routes/admin'); 
-// const cartRouter = require('./routes/cart');
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const fridgeRoute = require('./routes/fridge'); 
+const usersRouter = require('./routes/users'); 
+const adminRouter = require('./routes/admin'); 
+const cartRouter = require('./routes/cart');
 
-// require('./db');  //connection to Database
+require('./db');  //connection to Database
 
-// const app = express();
-// const PORT = process.env.PORT || 3000;
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 
-// // app.use(cors());
+// //app.use(cors());
 // const corsOptions = {
 //   origin: 'https://smartfridge-client.onrender.com',
 //   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
@@ -22,27 +22,22 @@
 
 // app.use(cors(corsOptions));
 // app.options('*', cors(corsOptions));
-
-// app.use(bodyParser.json());
-// app.use('/api/fridge', fridgeRoute); 
-// app.use('/api/users', usersRouter);  
-// app.use('/api/admin', adminRouter);
-// app.use('/api/cart', cartRouter);
+app.use(cors());
 
 
 
-// app.listen(PORT, () => {
-// console.log(`Server is running. Try accessing via Render URL in production or http://localhost:${PORT} locally.`);
-// });
+app.use(bodyParser.json());
+app.use('/api/fridge', fridgeRoute); 
+app.use('/api/users', usersRouter);  
+app.use('/api/admin', adminRouter);
+app.use('/api/cart', cartRouter);
 
-const express = require('express');
-const app = express();
-const PORT = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('✅ Server is working correctly.');
-});
 
 app.listen(PORT, () => {
-  console.log(`🟢 Server running on http://localhost:${PORT}`);
+console.log(`Server is running. Try accessing via Render URL in production or http://localhost:${PORT} locally.`);
 });
+
+
+
+
